8337b1410514c38bd6feff106b9fd57deb08eeb8
mosa-dev <prominsanmdg@gmail.com>
2026-09-14 10:56:44 +0300

    Update greeting name

--- app/src/main/java/app/mosa/expensetracker/MainActivity.kt
+++ app/src/main/java/app/mosa/expensetracker/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             ExpenseTrackerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "ExpenseTracker",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
